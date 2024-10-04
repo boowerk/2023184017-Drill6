0d39d8ae49586f611dfa3dceb66089ea6a2a5b88
boowerk <back20044@gmail.com>
2024-10-04 18:20:58 +0900

    기본적인 틀 제작 완료

---
+++ follow_hand_boy.py
@@ -0,0 +1,20 @@
+from pico2d import *
+
+open_canvas()
+
+character = load_image('animation_sheet.png')
+background = load_image('TUK_GROUND.png')
+hand = load_image('hand_arrow.png')
+
+def handle_events():
+
+    pass
+
+running = True
+
+while running:
+    
+
+    pass
+
+close_canvas()
