84db16f1b48960e81d53e43f3c96fd2494d7aa75
boowerk <back20044@gmail.com>
2024-10-04 18:22:22 +0900

    배경화면 구현 완료

--- follow_hand_boy.py
+++ follow_hand_boy.py
@@ -8,12 +8,19 @@ hand = load_image('hand_arrow.png')
 
 def handle_events():
 
+
+
     pass
 
 running = True
 
 while running:
-    
+
+    clear_canvas()
+    background.clip_draw(0, 0, 1200, 1800, 400, 300, 800, 600)
+
+    update_canvas()
+    handle_events()
 
     pass
 
