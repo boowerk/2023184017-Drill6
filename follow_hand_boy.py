3474418b11592cd4ab28af4b037e0e59166d870c
boowerk <back20044@gmail.com>
2024-10-04 18:25:49 +0900

    ESC 및 윈도우창 X를 누를시 종료 구현 완료

--- follow_hand_boy.py
+++ follow_hand_boy.py
@@ -8,7 +8,14 @@ hand = load_image('hand_arrow.png')
 
 def handle_events():
 
-
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
 
     pass
 
