77dbed3367568eef69068b8acb0a95e2fd7a5365
boowerk <back20044@gmail.com>
2024-10-04 18:40:54 +0900

    손을 따라가는 캐릭터 구현 완료

--- follow_hand_boy.py
+++ follow_hand_boy.py
@@ -1,5 +1,7 @@
-from pico2d import *
+from cgitb import reset
 
+from pico2d import *
+import random
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
@@ -21,18 +23,54 @@ def handle_events():
 
     pass
 
+def reset_hand_position():
+    global hand_x, hand_y
+    hand_x = random.randint(0, TUK_WIDTH)
+    hand_y = random.randint(0, TUK_HEIGHT)
+
+    pass
+
+
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
+direction = 1
+speed = 5
+
+reset_hand_position()
 
 while running:
 
     clear_canvas()
     background.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    hand.draw(hand_x, hand_y)
+
+    if x < hand_x:
+        x += speed
+        direction = 1
+    elif x > hand_x:
+        x -= speed
+        direction = -1
+
+    if y < hand_y:
+        y += speed
+    elif y > hand_y:
+        y -= speed
+
+    if abs(x - hand_x) < 10 and abs(y - hand_y) < 10:
+        reset_hand_position()
+
+    if direction == 1:
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    else:
+        character.clip_draw(frame * 100, 100 * 0, 100, 100, x, y)
 
     update_canvas()
     handle_events()
+
+    frame = (frame + 1) % 8
+
+    delay(0.01)
 
     pass
 
